9f0d7c98430536ad47583c5350e7f3c4fc27ca2a
Cour <coolmalhotraprankur@gmail.com>
2021-07-11 18:26:49 -0500

    Initial Commit

---
+++ cloudnotesapp/cloudnotesappApp.swift
@@ -0,0 +1,17 @@
+//
+//  cloudnotesappApp.swift
+//  cloudnotesapp
+//
+//  Created by Prankur Malhotra on 7/11/21.
+//
+
+import SwiftUI
+
+@main
+struct cloudnotesappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
